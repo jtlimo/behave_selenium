df2403d20c3eb245e973fbe046420b6b58388b85
Jessica <jlima@thoughtworks.com>
2017-04-26 15:43:37 -0300

    test

--- listener_exit_code.sh
+++ listener_exit_code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose --file=docker-compose.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
-    if [ "$code" == "1" ]; then
+    if ("$code" == "1"); then
        exit -1
     fi
 done
