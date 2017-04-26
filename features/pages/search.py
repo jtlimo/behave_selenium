981f10a69cfe345af81caffa85c2ac93d8e4308d
Jessica <jlima@thoughtworks.com>
2017-04-26 18:19:34 -0300

    fails a test

--- features/pages/search.py
+++ features/pages/search.py
@@ -1,8 +1,8 @@
-class Search():
+#class Search():
     #input_search = driver.find_element(:name, 'q')
 
-def search(self):
+#def search(self):
     #input_search.send_keys 'Teste'
 
-def validate(self):
+#def validate(self):
     #result = input_search.text
